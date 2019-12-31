366ded92c0d7830cab20e18fccfb1834b799cdfc
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-31 08:53:41 -0600

    Close nav on scroll

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ const toggleNav = () => {
   document.querySelector('.modal').classList.toggle('open');
 }
 
-const addEventListenerToNav = () => {
+const closeNav = () => {
+  const nav = document.querySelector('.modal');
+  if (nav.classList.contains('open')){
+    nav.classList.toggle('open');
+  }
+}
+
+const addEventListeners = () => {
   document.querySelector('.menu-icon').addEventListener('click', toggleNav);
   document.querySelector('.close-icon').addEventListener('click', toggleNav);
+  document.onscroll = closeNav;
 } 
 
-document.addEventListener('DOMContentLoaded', addEventListenerToNav)
+document.addEventListener('DOMContentLoaded', addEventListeners)
 
 ReactDOM.render(<App/>, document.querySelector('.calendar-controls'));
