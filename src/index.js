f1df530a0614e0f93925234e2d85155288c848ac
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-12-31 02:05:53 -0600

    Write ReactDOM render statement, toggle nav function, add eventlisteners to nav in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import './stylesheets/main.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const toggleNav = () => {
+  document.querySelector('.modal').classList.toggle('open');
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const addEventListenerToNav = () => {
+  document.querySelector('.menu-icon').addEventListener('click', toggleNav);
+  document.querySelector('.close-icon').addEventListener('click', toggleNav);
+} 
+
+document.addEventListener('DOMContentLoaded', addEventListenerToNav)
+
+ReactDOM.render(<App/>, document.querySelector('.calendar-controls'));
